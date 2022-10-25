9ceb462e3d4229f299061bd203d54252f713846c
7oSkaaa <ahmed.7oskaa@gmail.com>
2022-10-25 19:43:07 +0200

    edit: do try and catch in Student.views.py

--- student/views.py
+++ student/views.py
@@ -24,37 +24,45 @@ class StudentView(View):
         if students:
             return JsonResponse({'students': list(students.values())}, safe=False)
         else:
-            return JsonResponse({'message': 'No students found!'}, status=404)
+            return JsonResponse({'message': 'No students found!'}, status=422)
             
     # POST a new student
     def post(self, request):
-        form = StudentForm(data=json.loads(request.body))
-        if form.is_valid():
-            form.save()
-            return JsonResponse({'message': 'Student created successfully!'})
-        else:
-            return JsonResponse({'message': 'Student not created!'}, status=422)
-    
+        try:
+            form = StudentForm(data=json.loads(request.body))
+            if form.is_valid():
+                form.save()
+                return JsonResponse({'message': 'Student created successfully!'})
+            else:
+                return JsonResponse({'message': 'Student not created!'}, status=422)
+        except:
+            return JsonResponse({'message': 'Format error!'}, status=422)
     
 class StudentViewId(View):
     
     # GET a student by id
     def get(self, request, *args, **kwargs):
-        student = Student.objects.get(id=kwargs['id'])
-        if student:
-            return JsonResponse({'Student': toJson(student)}, safe=False)
-        else:
-            return JsonResponse({'message': 'Student not found!'}, status=404)
-    
+        try:
+            student = Student.objects.get(id=kwargs['id'])
+            if student:
+                return JsonResponse({'Student': toJson(student)}, safe=False)
+            else:
+                return JsonResponse({'message': 'Student not found!'}, status=422)
+        except:
+            return JsonResponse({'message': 'Format error!'}, status=422)
+            
     # PUT a student by id
     def put(self, request, *args, **kwargs):
-        form = StudentForm(data=json.loads(request.body), instance=Student.objects.get(id=kwargs['id']))
-        if form.is_valid():
-            form.save()
-            return JsonResponse({'message': 'Student updated successfully!'})
-        else:
-            return JsonResponse({'message': 'Student not updated!'}, status=422)
-    
+        try:
+            form = StudentForm(data=json.loads(request.body), instance=Student.objects.get(id=kwargs['id']))
+            if form.is_valid():
+                form.save()
+                return JsonResponse({'message': 'Student updated successfully!'})
+            else:
+                return JsonResponse({'message': 'Student not updated!'}, status=422)
+        except:
+            return JsonResponse({'message': 'Format error!'}, status=422)
+        
     # DELETE a student by id
     def delete(self, request, *args, **kwargs):
         student = Student.objects.filter(id=kwargs['id'])
@@ -62,4 +70,4 @@ class StudentViewId(View):
             student.delete()
             return JsonResponse({'message': 'Student deleted successfully!'})
         else:
-            return JsonResponse({'message': 'Student not found!'}, status=404)
+            return JsonResponse({'message': 'Student not found!'}, status=422)
