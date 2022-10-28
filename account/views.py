e4b152760464094172d0b252dbc362be3ae4e5f7
7oSkaaa <ahmed.7oskaa@gmail.com>
2022-10-28 15:44:25 +0200

    handle account not registered errors

--- account/views.py
+++ account/views.py
@@ -9,6 +9,7 @@ from datetime import datetime
 import jwt
 
 class Register(generics.GenericAPIView, mixins.CreateModelMixin):
+    
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
     
@@ -16,17 +17,30 @@ class Register(generics.GenericAPIView, mixins.CreateModelMixin):
         return self.create(request)
 
 class SignIn(generics.GenericAPIView, mixins.UpdateModelMixin):
+    
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
     
     def put(self, request):
         try:
-            token = jwt.encode({'username': request.data['username'], 'password': request.data['password'], 'timestamp': datetime.timestamp(datetime.now())}, 'secret', algorithm='HS256')
+            # get request data
+            [username, password, parent_id] = [request.data['username'], request.data['password'], request.data['parent']] 
+            
+            # trying to get account by username and password
+            account = Account.objects.get(username=username, password=password, parent = Parent.objects.get(id=parent_id))
+            
+            # generate token
+            token = jwt.encode({'username': username, 'password': password, 'timestamp': datetime.timestamp(datetime.now())}, 'secret', algorithm='HS256')
+            
+            # generate token object
             newToken = {
                 'token': token,
-                'parent': Parent.objects.get(id=request.data['parent'])
+                'parent': Parent.objects.get(id=parent_id)
             }
+            
+            # save token in database
             Tokens.objects.create(**newToken)
+            
             return Response({'token': token}, status=status.HTTP_200_OK)
         except Account.DoesNotExist:
             return Response({'message': 'Parent not registered!'}, status=status.HTTP_404_NOT_FOUND)
